26143a26b70a69a0bcefbba94aca9d715185f822
kalyanAIML <kalyan.erpln@gmail.com>
2026-04-20 13:43:42 +0530

    Create cidr-block.ts

---
+++ ec2/cidr-block.ts
@@ -0,0 +1,10 @@
+// ec2/cidr-block.ts
+
+const vpcCidr = "10.0.0.0/16";
+
+const subnet1 = "10.0.1.0/24";
+const subnet2 = "10.0.2.0/24";
+
+console.log("VPC CIDR Block:", vpcCidr);
+console.log("Public Subnet CIDR:", subnet1);
+console.log("Private Subnet CIDR:", subnet2);
